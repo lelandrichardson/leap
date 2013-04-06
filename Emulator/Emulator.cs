1c06a29c37a5f72d1655d55761b3450102c03217
Leland <leland.richardson@gmail.com>
2013-04-06 13:39:10 -0500

    added emulator project

---
+++ Emulator/Emulator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leap;
+
+namespace Emulator
+{
+    public class EmulatorController : Controller
+    {
+
+        public new bool AddListener(Listener listener)
+        {
+            
+            //TODO:
+            return true;
+        }
+        public new bool RemoveListener(Listener listener)
+        {
+            //TODO:
+            return true;
+        }
+
+
+        public void EmulateFromFile(string filepath)
+        {
+            //TODO:
+        }
+
+        public new void Dispose()
+        {
+            base.Dispose();
+        }
+
+    }
+
+    //class SampleProgram
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var listener = new Listener();
+    //        var controller = new EmulatorController();
+    //        controller.AddListener(listener);
+
+
+    //        controller.EmulateFromFile(@"C:\testleapfile.json");
+
+
+
+
+    //        controller.RemoveListener(listener);
+    //        controller.Dispose();
+    //    }
+    //}
+}
---
+++ TestConsoleApp/TestConsoleApp/GestureListener.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leap;
+
+namespace TestConsoleApp
+{
+    public class GestureListener : Listener
+    {
+        private Object thisLock = new Object();
+
+
+        public override void OnFrame(Controller controller)
+        {
+            var frame = controller.Frame();
+
+            if (!frame.Hands.Empty)
+            {
+                
+            }
+
+        }
+
+        
+        private GestureHandlerCollection handlerCollection = new GestureHandlerCollection();
+
+        public string On(string eventName, Action<object> callback)
+        {
+            return handlerCollection.Register(eventName, callback);
+        }
+
+        public IEnumerable<string> On(IEnumerable<string> eventNames, Action<object> callback)
+        {
+            var guids = new List<string>();
+            foreach (var eventName in eventNames)
+            {
+                guids.Add(handlerCollection.Register(eventName, callback));
+            }
+            return guids;
+        }
+
+        private void Off(int id)
+        {
+            //unbind
+
+        }
+
+
+
+
+
+    }
+
+    public class GestureHandlerCollection : Dictionary<string, List<Tuple<string,Action<object>>>>
+    {
+        public string Register(string eventName, Action<object> handler)
+        {
+            var guid = Guid.NewGuid().ToString();
+            if (!ContainsKey(eventName))
+            {
+                Add(eventName, new List<Tuple<string, Action<object>>>());
+            }
+            base[eventName].Add(new Tuple<string, Action<object>>(guid, handler));
+            return guid;
+        }
+
+        public void CallAll(string eventName, object p)
+        {
+            if (!ContainsKey(eventName)) return;
+
+            var handlers = base[eventName];
+            foreach (var handler in handlers)
+            {
+                handler.Item2(p);
+            }
+        }
+    }
+}
